cac6c6371306bf8a167a44d96dd9d89859ba8623
LArellano933 <arellanoleonidas1@gmail.com>
2026-03-29 22:05:42 -0400

    Create Blackjack.h
    Added header file for Blackjack game class.

---
+++ include/Blackjack.h
@@ -0,0 +1,10 @@
+// Header file for Blackjack minigame/Blackjack game class.
+// Related Files: Blackjack.cpp, Player.h
+// Date Created: 3/29/2026
+// Last Edited: 3/29/2026
+
+#pragma once
+#include "Player.h"
+
+class Blackjack {
+};
