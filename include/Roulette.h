f5271dd8d93ad47ef3cc560a0cb480263ed82c6f
LArellano933 <arellanoleonidas1@gmail.com>
2026-03-29 22:12:38 -0400

    Create Roulette.h
    Added header file for Roulette class.

---
+++ include/Roulette.h
@@ -0,0 +1,10 @@
+// Header file for Roulette minigame/Roulette game class.
+// Related Files: Roulette.cpp, Player.h
+// Date Created: 3/29/2026
+// Last Edited: 3/29/2026
+
+#pragma once
+#include "Player.h"
+
+class Roulette {
+};
