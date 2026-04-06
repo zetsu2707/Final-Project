6ed07411c0ca94d6fbe67684ba06a198d0732c65
LArellano933 <arellanoleonidas1@gmail.com>
2026-04-05 20:16:41 -0400

    Roulette impl logic
    Added Roulette class with methods for gameplay and betting.

--- include/Roulette.h
+++ include/Roulette.h
@@ -1,11 +1,32 @@
 // Header file for Roulette minigame/Roulette game class.
 // Related Files: Roulette.cpp, Player.h
 // Date Created: 3/29/2026
-// Last Edited: 3/29/2026
+// Last Edited: 04/05/2026
 
 #pragma once
-#include "CasinoGame.h"
 #include "Player.h"
+#include <vector>
+#include <string>
+#include <random>
 
 class Roulette {
+public:
+    Roulette();
+
+    void play(Player& player);
+
+private:
+    struct Bet {
+        std::string type;
+        int number;
+        double amount;
+    };
+
+    int spinWheel();
+    bool isRed(int number) const;
+    void showBanner() const;
+    double getBetAmount(const Player& player) const;
+    int getValidatedNumber(int min, int max) const;
+
+    std::mt19937 m_rng;
 };
