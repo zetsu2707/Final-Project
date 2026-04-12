7a74e2c3f93e3e8c097f1178beab5cc06305f6cb
LArellano933 <arellanoleonidas1@gmail.com>
2026-04-12 18:49:05 -0400

    Blackjack header update for UI

--- include/Blackjack.h
+++ include/Blackjack.h
@@ -1,10 +1,9 @@
 // Header file for Blackjack minigame/Blackjack game class.
-// Related Files: Blackjack.cpp, Player.h
+// Related Files: Blackjack.cpp, Player.h, BlackjackUI.h
 // Date Created: 3/29/2026
-// Last Edited: 4/04/2026
+// Last Edited: 4/12/2026
 
 #pragma once
-#include "CasinoGame.h"
 #include "Player.h"
 #include <string>
 #include <vector>
@@ -14,10 +13,53 @@ class Blackjack {
 public:
     Blackjack();
 
-    void play(Player& player);
+    enum class RoundState {
+        WaitingForBet,
+        PlayerTurn,
+        DealerTurn,
+        RoundOver
+    };
+
+    enum class RoundResult {
+        None,
+        PlayerBlackjack,
+        DealerBlackjack,
+        PlayerBust,
+        DealerBust,
+        PlayerWin,
+        DealerWin,
+        Push
+    };
+
+    struct CardView {
+        std::string rank;
+        std::string suit;
+        int value;
+    };
+
+    bool startRound(Player& player, double bet);
+    void playerHit(Player& player);
+    void playerStand(Player& player);
+    void resetRound();
+
+    RoundState getRoundState() const;
+    RoundResult getRoundResult() const;
+
+    std::vector<CardView> getPlayerCards() const;
+    std::vector<CardView> getDealerCards() const;
+
+    int getPlayerValue() const;
+    int getDealerValue() const;
+    int getDealerVisibleValue() const;
+
+    double getCurrentBet() const;
+    std::string getStatusText() const;
+
+    bool isRoundOver() const;
+    bool isDealerHoleCardHidden() const;
+    bool isPlayerTurn() const;
 
 private:
-
     struct Card {
         std::string rank;
         std::string suit;
@@ -29,7 +71,7 @@ private:
         void addCard(const Card& card);
         int getValue() const;
         void clear();
-        void show(bool hideFirstCard = false) const;
+        const std::vector<Card>& getCards() const;
 
     private:
         std::vector<Card> m_cards;
@@ -56,7 +98,8 @@ private:
 
         void hit(Deck& deck);
         int getHandValue() const;
-        void showHand(bool hideFirstCard = false) const;
+        const Hand& getHand() const;
+        void clearHand();
 
     protected:
         Hand m_hand;
@@ -64,7 +107,7 @@ private:
 
     class BJPlayer : public Participant {
     public:
-        void takeTurn(Deck& deck) override; 
+        void takeTurn(Deck& deck) override;
     };
 
     class Dealer : public Participant {
@@ -72,9 +115,16 @@ private:
         void takeTurn(Deck& deck) override;
     };
 
-    void showBanner() const;
-    double getBet(const Player& player) const;
+    void settleRound(Player& player);
+    std::vector<CardView> toCardViews(const Hand& hand) const;
 
-    template <typename T>
-    T getValidatedInput(T min, T max) const;
+    Deck m_deck;
+    BJPlayer m_playerHand;
+    Dealer m_dealerHand;
+
+    double m_currentBet;
+    RoundState m_roundState;
+    RoundResult m_roundResult;
+    std::string m_statusText;
+    bool m_hideDealerHoleCard;
 };
